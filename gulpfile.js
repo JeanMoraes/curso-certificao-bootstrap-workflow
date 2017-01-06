5ea7dcf573954749800300a6d99c8ac6f57afcdc
Keven jesus <keven@Keven-MacBook-Pro.local>
2017-01-06 14:04:48 -0200

    plugin sass para compilar

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-var gulp = require("gulp");
-var cleanCSS = require('gulp-clean-css');
-var htmlmin = require('gulp-htmlmin');
+var gulp 			= require("gulp");
+var cleanCSS 	= require('gulp-clean-css');
+var htmlmin 	= require('gulp-htmlmin');
+var sass 			= require("gulp-sass");
 
 gulp.task('minificar-css', function() {
   return gulp.src('./source/scss/*.scss')
+  		.pipe(sass())
     	.pipe(cleanCSS({compatibility: 'ie8'}))
-    	.pipe(gulp.dest('./dist/scss'));
+    	.pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('minificar-html', function() {
